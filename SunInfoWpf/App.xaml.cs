dd39f4e9d59deb0ff974550dad82699b4d3c49d4
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-18 23:21:01 +0200

    Set up dependency injection

--- SunInfoWpf/App.xaml.cs
+++ SunInfoWpf/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using CoreLibrary;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SunInfoWpf
 {
@@ -13,5 +15,20 @@ namespace SunInfoWpf
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<ILatitudeValidator, LatitudeValidator>();
+            services.AddSingleton<ILongitudeValidator, LongitudeValidator>();
+        }
     }
 }
