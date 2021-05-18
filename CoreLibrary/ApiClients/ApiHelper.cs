7f41f93e9cd6462c1177c1e11a20483f38f125fe
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-18 23:52:54 +0200

    Class for initializing an ApiClient

---
+++ CoreLibrary/ApiClients/ApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace CoreLibrary.ApiClients
+{
+    public class ApiHelper
+    {
+        public HttpClient HttpClient { get; set; }
+
+        public ApiHelper()
+        {
+            HttpClient = new HttpClient();
+
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
