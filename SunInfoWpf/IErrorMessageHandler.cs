bc92a1962e89bd439757cb37ba0643084f11cb2d
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-18 23:36:03 +0200

    Interface for error message handler

---
+++ SunInfoWpf/IErrorMessageHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunInfoWpf
+{
+    public interface IErrorMessageHandler
+    {
+        void AddMessage(string message);
+        string GetMessages();
+    }
+}
