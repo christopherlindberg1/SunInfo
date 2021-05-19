eeb0215b79b3a3f4e34e68e56aeb5056fe11ffcd
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-19 10:58:47 +0200

    Added property for SunDataModel

--- CoreLibrary/Models/SunResultModel.cs
+++ CoreLibrary/Models/SunResultModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CoreLibrary.Models
 {
-    class SunResultModel
+    public class SunResultModel
     {
+        public SunDataModel SunDataModel { get; set; }
     }
 }
