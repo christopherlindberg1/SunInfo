5e94be67d5537ae35b6ba22e1680b6c6663891e2
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-19 10:56:22 +0200

    Implemented method for building url for API call

---
+++ CoreLibrary/ApiClients/SunApiClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreLibrary.ApiClients
+{
+    public class SunApiClient
+    {
+        private string _baseUrl = "https://api.sunrise-sunset.org/json";
+
+        private string BuildUrl(string latitude, string longitude, DateTime? date)
+        {
+            StringBuilder url = new StringBuilder(_baseUrl);
+
+            url.Append($"?lat={ latitude }");
+            url.Append($"&lng={ longitude }");
+
+            if (date != null)
+            {
+                url.Append($"&date={ date.Value.Date }");
+            }
+
+            return url.ToString();
+        }
+
+        
+    }
+}
