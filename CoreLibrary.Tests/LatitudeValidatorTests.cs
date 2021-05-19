aff47315daed69225600255af38a1227f9867f58
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-19 20:50:21 +0200

    Test for ILatitudeValidator: IsValidLatitude_CorrectFormatInvalidValues_ReturnsFalse()

---
+++ CoreLibrary.Tests/LatitudeValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CoreLibrary.Tests
+{
+    public class LatitudeValidatorTests
+    {
+        [Theory]
+        [InlineData("90.0001")]
+        [InlineData("-90.0001")]
+        [InlineData("900.0001")]
+        [InlineData("-900.0001")]
+        [InlineData("91.1")]
+        [InlineData("-91.1")]
+        [InlineData("91")]
+        [InlineData("-91")]
+        public void IsValidLatitude_CorrectFormatInvalidValues_ReturnsFalse(string latitude)
+        {
+            // Arrange
+            ILatitudeValidator validator = new LatitudeValidator();
+            bool expected = false;
+
+            // Act
+            bool actual = validator.IsValidLatitude(latitude);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
