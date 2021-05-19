9203812b027a3a6d6ed98cd27b70a0cadb000e73
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-19 12:23:52 +0200

    Implements IApiHelper

--- CoreLibrary/ApiClients/ApiHelper.cs
+++ CoreLibrary/ApiClients/ApiHelper.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace CoreLibrary.ApiClients
 {
-    public class ApiHelper
+    public class ApiHelper : IApiHelper
     {
         public HttpClient HttpClient { get; set; }
 
