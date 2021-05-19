dc56a0b745482e176ddc9e1448646b6a182d2f1e
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-19 12:24:39 +0200

    Interface for a sun api client class

---
+++ CoreLibrary/ApiClients/ISunApiClient.cs
@@ -0,0 +1,11 @@
+﻿using CoreLibrary.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace CoreLibrary.ApiClients
+{
+    public interface ISunApiClient
+    {
+        Task<SunDataModel> GetSunInformation(string latitude, string longitude, DateTime? date);
+    }
+}
