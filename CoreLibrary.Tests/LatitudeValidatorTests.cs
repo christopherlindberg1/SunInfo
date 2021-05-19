ddd3436fb3fb61a1dc116d78d6b354354b84de12
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-19 20:58:13 +0200

    Test for ILatitudeValidator: IsValidLatitude_IncorrectFormat_ReturnsFalse()

--- CoreLibrary.Tests/LatitudeValidatorTests.cs
+++ CoreLibrary.Tests/LatitudeValidatorTests.cs
@@ -28,5 +28,41 @@ namespace CoreLibrary.Tests
             // Assert
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData("10.")]
+        [InlineData("10,")]
+        [InlineData("m.41")]
+        [InlineData("70,5")]
+        [InlineData("70.7854m")]
+        [InlineData("70m.7854m")]
+        [InlineData("m70.7854m")]
+        [InlineData("m70.m7854")]
+        [InlineData("70.-7854")]
+        [InlineData("70,-7854")]
+        [InlineData(".7854")]
+        [InlineData(",7854")]
+        [InlineData(".7854m")]
+        [InlineData(",7854m")]
+        [InlineData("m.m")]
+        [InlineData("m,m")]
+        [InlineData("m.")]
+        [InlineData("m,")]
+        [InlineData(".m")]
+        [InlineData(",m")]
+        [InlineData(".")]
+        [InlineData(",")]
+        public void IsValidLatitude_IncorrectFormat_ReturnsFalse(string latitude)
+        {
+            // Arrange
+            ILatitudeValidator validator = new LatitudeValidator();
+            bool expected = false;
+
+            // Act
+            bool actual = validator.IsValidLatitude(latitude);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
