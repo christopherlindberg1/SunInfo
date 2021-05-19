3faca36fb1faf723dae05452d3665f385dc6a7f7
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-05-19 12:24:15 +0200

    Interface for an api helper class

---
+++ CoreLibrary/ApiClients/IApiHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CoreLibrary.ApiClients
+{
+    public interface IApiHelper
+    {
+        HttpClient HttpClient { get; set; }
+    }
+}
